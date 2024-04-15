86807c04525500f4264325c187e3abadf37502fe
eungwang1 <scm323@naver.com>
2024-04-16 08:30:33 +0900

    feat: footer 추가

--- src/app/(routes)/_components/home-page.tsx
+++ src/app/(routes)/_components/home-page.tsx
@@ -6,15 +6,19 @@ import KeywordTab from "./keyword-tab";
 import BlogAnalysisTab from "./blog-analysis-tab";
 import "../_lib/styles/home-page.css";
 import HomeHeader from "./home-header";
+import HomeFooter from "./home-footer";
 
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = () => {
   return (
     <Suspense>
-      <HomeHeader />
-      <div className="max-w-screen-xl mx-auto p-5">
-        <Tabs items={tabItems} />
+      <div className="min-h-screen flex flex-col">
+        <HomeHeader />
+        <div className="max-w-screen-xl mx-auto p-5 w-full mb-16">
+          <Tabs items={tabItems} />
+        </div>
+        <HomeFooter />
       </div>
     </Suspense>
   );
