782d4d57181a9890a41d903482ca6c9a5936e00b
eungwang <scm323@naver.com>
2024-04-16 01:04:14 +0900

    feat: 카카오톡 오픈채팅 이동링크 추가

--- src/app/(routes)/_components/home-header.tsx
+++ src/app/(routes)/_components/home-header.tsx
@@ -1,13 +1,33 @@
+import { Tooltip } from "antd";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface HomeHeaderProps {}
 
 const HomeHeader: React.FC<HomeHeaderProps> = () => {
   return (
-    <div className="bg-blue-400 text-white py-5">
-      <div className="max-w-screen-xl w-full flex items-end gap-2 justify-start mx-auto px-4">
-        <h1 className="text-2xl font-bold">비에이</h1>
-        <p>네이버 블로그 분석 서비스</p>
+    <div className="bg-blue-400 text-white py-4 ">
+      <div className="flex justify-between max-w-screen-xl mx-auto px-4 items-center">
+        <div className="w-full flex items-end gap-2 justify-start">
+          <h1 className="text-2xl font-bold">비에이</h1>
+          <p>네이버 블로그 분석 서비스</p>
+        </div>
+        <Tooltip title="이용자 단톡방">
+          <Link
+            className="-scale-x-100"
+            href="https://open.kakao.com/o/gcyfdlmg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/images/home/kakao.svg"
+              width={30}
+              height={30}
+              alt="logo"
+            />
+          </Link>
+        </Tooltip>
       </div>
     </div>
   );
