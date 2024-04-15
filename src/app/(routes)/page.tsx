a16a71a8e424bdeafdb1c063bf576ab5e0bdc296
eungwang <scm323@naver.com>
2024-04-16 00:27:49 +0900

    feat: 메타태그 설정

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,4 +1,13 @@
+import { Metadata } from "next";
 import HomePage from "./_components/home-page";
+
+export const metadata: Metadata = {
+  title: "비에이 | Blog Analysis",
+  description: "네이버 블로그 분석 서비스 비에이",
+  keywords:
+    "비에이, 웨어이즈포스트, 블덱스, 블로그지수, 블로그지수사이트, 블로그누락확인, 블로그노출, 블로그분석",
+  icons: "/favicon.ico",
+};
 
 export default function Home() {
   return <HomePage />;
